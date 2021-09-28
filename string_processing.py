cf6bd034f4098479116dac23912082f156b51c39
Tucker Komasa <tskomasa@gmail.com>
2021-09-27 19:53:57 -0700

    - updated

--- string_processing.py
+++ string_processing.py
@@ -1,15 +1,21 @@
 # imports
 from PIL import Image, ImageEnhance
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Tucker\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 # assigning an image from the source path
-img = Image.open('''image_test\\test.png''')
+img = Image.open('''image_test\\pytest.png''')
+
+# adding some sharpness and contrast to the image
+enhancer1 = ImageEnhance.Sharpness(img)
+enhancer2 = ImageEnhance.Contrast(img)
+img_edit = enhancer1.enhance(20.0)
+img_edit = enhancer2.enhance(1.5)
+
+# save the new image
+img_edit.save("imeage_test\\edited_image.png")
 
 # converts the image to result and saves it into result variable
-result = pytesseract.image_to_string(img)
-
-with open('text_result2.txt', mode ='w') as file:
- file.write(result)
- print("ready!")
-
+result = pytesseract.image_to_string(img_edit)
+with open('text_result2.txt', mode='w') as file:
+    file.write(result)
+    print("File ready.")
